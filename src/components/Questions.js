f582c1a50e2f2c14a8a72f7843521343ff074b81
AndresHadechine <andres.hadechine@sofka.com.co>
2022-02-15 11:00:25 -0500

    correción de preguntas y cambio de Cards por Buttons

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,35 +1,39 @@
 import React, { useEffect, useState } from "react";
 import Modal from "react-bootstrap/Modal";
-import Card from "react-bootstrap/Card";
-import Table from "react-bootstrap/Table";
 import "./assets/css/Trivia.css";
+import Button from 'react-bootstrap/Button';
 
 const Questions = () => {
     const [todos, setTodos] = useState([]);
+    const [i, setI] = useState(0);
     const [question, setQuestion] = useState();
     const [answer1, setAnswer1] = useState();
     const [answer2, setAnswer2] = useState();
     const [answer3, setAnswer3] = useState();
     const [answer4, setAnswer4] = useState();
 
-
   const consumeApi = async () => {
-    const url = "https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple";
+    const url = "https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=multiple";
     const response = await fetch(url);
     console.log(response.statusText);
     const responseJSON = await response.json();
     setTodos(responseJSON);
     console.log(responseJSON);
-    setQuestion(responseJSON.results[1].question);
-    setAnswer1(responseJSON.results[1].incorrect_answers[0]);
-    setAnswer2(responseJSON.results[1].incorrect_answers[1]);
-    setAnswer3(responseJSON.results[1].incorrect_answers[2]);
-    setAnswer4(responseJSON.results[1].correct_answer);
+    setQuestion(responseJSON.results[i].question);
+    setAnswer1(responseJSON.results[i].incorrect_answers[0]);
+    setAnswer2(responseJSON.results[i].incorrect_answers[1]);
+    setAnswer3(responseJSON.results[i].incorrect_answers[2]);
+    setAnswer4(responseJSON.results[i].correct_answer);
+  };
+
+  const nextQuestion = async () => {
+    const j = (i+1);
+    setI(j);
 
   };
   useEffect(() => {
     consumeApi();
-  }, []);
+  }, [i]);
 
   return (
     <div>
@@ -38,44 +42,35 @@ const Questions = () => {
           <Modal.Title>Question</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>{todos.response_code}</p>
           <p><b>{question}</b></p>
         </Modal.Body>
         <Modal.Footer>
-          <Table variant="dark" className="row d-flex justify-content-center">
+          <table  className="row d-flex justify-content-center">
             <tr>
               <td>
-                <Card bg="danger" style={{ width: "22rem" }} className="mb-2">
-                  <Card.Body>
-                    <Card.Text className="text-white"><b>{answer1}</b></Card.Text>
-                  </Card.Body>
-                </Card>
+              <Button variant='danger' style={{ width: "22rem" }} className="mb-2" onClick={nextQuestion}>
+                   <b>{answer1}</b>
+              </Button>
               </td>
               <td>
-                <Card bg="info" style={{ width: "22rem" }} className="mb-2">
-                  <Card.Body>
-                    <Card.Text className="text-white"><b>{answer2}</b></Card.Text>
-                  </Card.Body>
-                </Card>
+              <Button variant="info" style={{ width: "22rem" }} className="mb-2" onClick={nextQuestion}>
+                   <b>{answer2}</b>
+              </Button>
               </td>
             </tr>
             <tr>
               <td>
-                <Card bg="warning" style={{ width: "22rem" }} className="mb-2">
-                  <Card.Body>
-                    <Card.Text className="text-white"><b>{answer3}</b></Card.Text>
-                  </Card.Body>
-                </Card>
+              <Button variant="warning" style={{ width: "22rem" }} className="mb-2" onClick={nextQuestion}>
+                   <b>{answer3}</b>
+              </Button>
               </td>
               <td>
-                <Card bg="success" style={{ width: "22rem" }} className="mb-2">
-                  <Card.Body>
-                    <Card.Text className="text-white"><b>{answer4}</b></Card.Text>
-                  </Card.Body>
-                </Card>
+              <Button variant="success" style={{ width: "22rem" }} className="mb-2" onClick={nextQuestion}>
+                   <b>{answer4}</b>
+              </Button>
               </td>
             </tr>
-          </Table>
+          </table>
         </Modal.Footer>
       </Modal.Dialog>
     </div>
